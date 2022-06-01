3fd4ef96b888a19dc8dad30a7e03ba2d093eb532
rollrat <rollrat.cse@gmail.com>
2022-06-01 10:07:20 +0900

    next components/scss/route test

---
+++ components/layouts/BottomNavigation.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import React, { ReactElement, useState } from "react";
+import { HomeIcon, SearchIcon, PencilIcon, HeartBreakIcon } from "../Icons";
+import styles from "./BottomNavigation.module.scss";
+
+type BottomNavigationProps = {
+  selected: number;
+  onChange?: (index: number) => void;
+};
+
+export default function BottomNavigationLayout({
+  selected,
+  onChange,
+}: BottomNavigationProps) {
+  const icons = [
+    <HomeIcon key={0} />,
+    <SearchIcon key={1} />,
+    <PencilIcon key={2} />,
+    <HeartBreakIcon key={3} />,
+    <HomeIcon key={4} />,
+  ];
+
+  const route = ["/", "/search", "/write", "/bookmark", "/profile"];
+
+  return (
+    <nav className={styles.BottomNavBox}>
+      <div className={styles.BottomNavBoxDivider} />
+      <div className={styles.BottomNavBoxIcons}>
+        {icons.map((icon, index) => (
+          <Link key={index} href={`${route[index]}`}>
+            <a
+              className={`${styles.BottomNavBoxIcon} ${
+                index === selected ? styles.Selected : ""
+              }`}
+            >
+              {icon}
+            </a>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+// BottomNavigationLayout.getLayout = function  getLayout() {
+//   return (
+//     <BottomNavigationLayout selected={0}/>
+//   )
+// }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { NextPage } from 'next'
+import { ReactElement, ReactNode } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// https://nextjs.org/docs/basic-features/layouts
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
