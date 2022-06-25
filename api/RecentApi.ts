94d71c231ce83ca779201ee28ef8889f8a766366
rollrat <rollrat.cse@gmail.com>
2022-06-25 13:33:05 +0900

    useRecentArticles 구현

---
+++ api/RecentApi.ts
@@ -0,0 +1,18 @@
+import useSWR from "swr";
+import { ArticleHeaderModel } from "../models/ArticleHeaderModel";
+import { ApiReturnType, fetcher } from "./ApiInterface";
+
+export default function useRecentArticles(
+  boardKey: string,
+  page: number
+): ApiReturnType<[ArticleHeaderModel]> {
+  const { data, error } = useSWR(`/api/article/recent`, fetcher, {
+    revalidateOnFocus: false,
+  });
+
+  return {
+    data: data["articles"] as [ArticleHeaderModel],
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
