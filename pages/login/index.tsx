8a62bb1685ae5afa16c2c1c249b31905c31e4208
rollrat <rollrat.cse@gmail.com>
2022-06-29 22:09:10 +0900

    feat(login): login 버튼 구현

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -11,6 +11,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AppBar from "../../components/AppBar";
 import styles from "./index.module.scss";
 import styled, { css } from "styled-components";
+import SelectButton from "../../components/SelectButton";
 
 // const loginPage = "http://localhost:8080/user/login";
 
@@ -136,6 +137,44 @@ const LoginTextBox = styled.input<LoginTextBoxProps>`
   } */
 `;
 
+const LoginButton = styled.input`
+  /* height: 31px;
+  border-radius: 10px;
+  background-color: #f5f5f5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+  width: 100%;
+  font: normal normal bold 12px Noto Sans KR;
+  color: #6b6f72; */
+  border: none;
+  outline: none;
+  background: none;
+
+  width: 320px;
+  height: 38px;
+  margin: 25px 20px 30px;
+  padding: 10px 25px;
+  border-radius: 5px;
+  background-color: #5673eb;
+
+  font-family: NotoSansCJKKR;
+  font-size: 12px;
+  font-weight: 500;
+  text-align: center;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #ededed;
+
+  :hover {
+    background-color: #b0bdf3;
+  }
+`;
+
 export default function LoginPage() {
   const user: UserDataType = useSelector(({ user }: any) => user);
 
@@ -144,14 +183,22 @@ export default function LoginPage() {
       <AppBar title={"로그인"} showUnderLine={true} />
       <div className={styles.LoginLogoBox}>
         <div className={styles.LoginLogo}>CANDY</div>
-        <LoginTextBox type="text" id="id" name="id" required />
+        <LoginTextBox
+          type="text"
+          id="id"
+          name="id"
+          required
+          placeholder="이메일 또는 휴대전화"
+        />
         <LoginTextBox
           type="password"
           id="password"
           name="password"
           required
           isPassword
+          placeholder="비밀번호"
         />
+        <LoginButton type="button" value={"로그인"} />
       </div>
     </SSRProvider>
   );
