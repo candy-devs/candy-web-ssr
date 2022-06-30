e8b958fb14fc9ff35562f386cd92667a5e4e59b6
rollrat <rollrat.cse@gmail.com>
2022-06-30 10:27:46 +0900

    chore(api): /user/mysinfo를 /info로 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,7 @@ MyApp.getInitialProps = wrapper.getInitialPageProps(
   (store) => async (ctx: any) => {
     const cookie = ctx?.ctx?.req?.headers?.cookie;
     if (cookie !== undefined) {
-      const uinfo = await axios.get("http://localhost:8080/api/user/info", {
+      const uinfo = await axios.get("http://localhost:8080/api/v1/user/info", {
         headers: {
           cookie: cookie!,
         },
