d6f5312773fb2dd2222e77642e2feeaa081cca32
rollrat <rollrat.cse@gmail.com>
2022-07-07 16:30:44 +0900

    chore(test): hello post test

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,27 +1,41 @@
-import { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import BottomSheet from '../components/BottomSheet';
-import * as counterActions from '../store/modules/counter';
+import axios from "axios";
+import { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import BottomSheet from "../components/BottomSheet";
+import { apiPrefix } from "../config/ApiConfig";
+import * as counterActions from "../store/modules/counter";
 
 export default function Test() {
-    const dispatch = useDispatch();
-    const value = useSelector(({ counter }: any) => counter.value);
+  const dispatch = useDispatch();
+  const value = useSelector(({ counter }: any) => counter.value);
 
-    const plus = useCallback(({ value }: any) => {
-        dispatch(counterActions.increment({ value }));
-    }, [dispatch]);
+  const plus = useCallback(
+    ({ value }: any) => {
+      dispatch(counterActions.increment({ value }));
+    },
+    [dispatch]
+  );
 
-    const minus = useCallback(({ value }: any) => {
-        dispatch(counterActions.decrement({ value }));
-    }, [dispatch]);
+  const minus = useCallback(
+    ({ value }: any) => {
+      dispatch(counterActions.decrement({ value }));
+    },
+    [dispatch]
+  );
 
-    return (
-        <div>
-            <h1>Counter</h1>
-            <button onClick={() => minus({ value })}>-</button>
-            <span>{value}</span>
-            <button onClick={() => plus({ value })}>+</button>
-            <BottomSheet/>
-        </div>
-    );
+  const postButton = useCallback(async () => {
+    const res = await axios.post(`${apiPrefix}/api/v1/hello`);
+    console.log(res.data);
+  }, []);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button onClick={() => minus({ value })}>-</button>
+      <span>{value}</span>
+      <button onClick={() => plus({ value })}>+</button>
+      <BottomSheet />
+      <button onClick={() => postButton()}>Test Hello Post</button>
+    </div>
+  );
 }
