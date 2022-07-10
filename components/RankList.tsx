5a37a28d634b78b00d615c878a972af81429364e
rollrat <rollrat.cse@gmail.com>
2022-07-10 18:12:12 +0900

    feat(hot): slick carousel 적용

--- components/RankList.tsx
+++ components/RankList.tsx
@@ -6,7 +6,11 @@ import React, {
   useRef,
 } from "react";
 import { useState } from "react";
+import styled from "styled-components";
 import styles from "./RankList.module.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 type RankListItemProps = {
   rank: string;
@@ -44,6 +48,7 @@ function RankListPage() {
   // ref.current.style.setProperty("--rank-list-page-width", `100px`);
 
   return (
+    // <div>
     <div className={styles.RankListPage} style={{ padding: "0" }}>
       <RankListItem rank="1" name="정치 사회" variance={20} />
       <RankListItem rank="2" name="코인" variance={30} />
@@ -56,95 +61,128 @@ function RankListPage() {
 }
 
 export default function RankList() {
-  const ref = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const scrollRef = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const [width, setWidth] = useState(0);
-  const [pageIndex, setPageIndex] = useState(0);
-
-  var onScrollTimer = -1;
-  var onMouse = false;
-  var onScrolling = false;
-  var onScrollLeft = 0;
-
-  useEffect(() => {
-    if (ref.current.getBoundingClientRect().width !== 0) {
-      const { style } = ref.current;
-      setWidth(ref.current.getBoundingClientRect().width);
-      style.setProperty(
-        "--rank-list-page-width",
-        `${ref.current.getBoundingClientRect().width}px`
-      );
-      if (scrollRef.current != null)
-       scrollRef.current.scrollLeft = Math.round(onScrollLeft / width) * width;
-      setPageIndex(Math.round(onScrollLeft / width));
-    }
-  }, [ref, scrollRef, onScrollLeft, width]);
-
-  const resize = () => {
-    const { style, clientWidth } = ref.current;
-    if (clientWidth !== 0) {
-      setWidth(clientWidth);
-      style.setProperty("--rank-list-page-width", `${clientWidth}px`);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", resize);
-    return () => {
-      window.removeEventListener("resize", resize);
-    };
-  }, [ref]);
-
-  const onScroll = function (event: SyntheticEvent<HTMLDivElement>) {
-    onScrolling = true;
-    if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
-    onScrollTimer = window.setTimeout(onScrollEnd, 100);
-    onScrollLeft = event.currentTarget.scrollLeft;
-  };
-
-  const onScrollEnd = () => {
-    if (onMouse === false && onScrolling) {
-      onScrolling = false;
-      scrollRef.current.scrollLeft = Math.round(onScrollLeft / width) * width;
-      setPageIndex(Math.round(onScrollLeft / width));
-    }
-  };
-
-  const onMouseDown = () => {
-    onMouse = true;
-    if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
-  };
-
-  const onMouseUp = () => {
-    onMouse = false;
-    if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
-    onScrollTimer = window.setTimeout(onScrollEnd, 100);
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    // autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
   return (
-    <div ref={ref} className={styles.RankListWrapper}>
-      <div
-        ref={scrollRef}
-        className={styles.RankList}
-        onScroll={onScroll}
-        onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}
-      >
+    <CarouselContainer className={styles.RankListPage}>
+      <Slider {...settings}>
         <RankListPage />
         <RankListPage />
         <RankListPage />
         <RankListPage />
-      </div>
-      <div className={styles.RankListDots}>
-        {[...Array(4).keys()].map((_, index) => (
-          <div
-            key={index}
-            className={`${styles.RankListDot} ${
-              index === pageIndex ? styles.Selected : styles.UnSelected
-            }`}
-          ></div>
-        ))}
-      </div>
-    </div>
+      </Slider>
+    </CarouselContainer>
   );
 }
+
+const CarouselContainer = styled.div`
+  .slick-dots li button:before {
+    transition-duration: 300ms;
+  }
+  padding: 0;
+  max-width: 100vw;
+  width: 100%;
+  overflow: hidden;
+  height: 150px;
+`;
+
+// export default function RankList() {
+//   const ref = useRef() as React.MutableRefObject<HTMLDivElement>;
+//   const scrollRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+//   const [width, setWidth] = useState(0);
+//   const [pageIndex, setPageIndex] = useState(0);
+
+//   var onScrollTimer = -1;
+//   var onMouse = false;
+//   var onScrolling = false;
+//   var onScrollLeft = 0;
+
+//   useEffect(() => {
+//     if (ref.current.getBoundingClientRect().width !== 0) {
+//       const { style } = ref.current;
+//       setWidth(ref.current.getBoundingClientRect().width);
+//       style.setProperty(
+//         "--rank-list-page-width",
+//         `${ref.current.getBoundingClientRect().width}px`
+//       );
+//       if (scrollRef.current != null)
+//         scrollRef.current.scrollLeft = Math.round(onScrollLeft / width) * width;
+//       setPageIndex(Math.round(onScrollLeft / width));
+//     }
+//   }, [ref, scrollRef, onScrollLeft, width]);
+
+//   const resize = () => {
+//     const { style, clientWidth } = ref.current;
+//     if (clientWidth !== 0) {
+//       setWidth(clientWidth);
+//       style.setProperty("--rank-list-page-width", `${clientWidth}px`);
+//     }
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener("resize", resize);
+//     return () => {
+//       window.removeEventListener("resize", resize);
+//     };
+//   }, [ref]);
+
+//   const onScroll = function (event: SyntheticEvent<HTMLDivElement>) {
+//     onScrolling = true;
+//     if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
+//     onScrollTimer = window.setTimeout(onScrollEnd, 100);
+//     onScrollLeft = event.currentTarget.scrollLeft;
+//   };
+
+//   const onScrollEnd = () => {
+//     if (onMouse === false && onScrolling) {
+//       onScrolling = false;
+//       scrollRef.current.scrollLeft = Math.round(onScrollLeft / width) * width;
+//       setPageIndex(Math.round(onScrollLeft / width));
+//     }
+//   };
+
+//   const onMouseDown = () => {
+//     onMouse = true;
+//     if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
+//   };
+
+//   const onMouseUp = () => {
+//     onMouse = false;
+//     if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
+//     onScrollTimer = window.setTimeout(onScrollEnd, 100);
+//   };
+
+//   return (
+//     <div ref={ref} className={styles.RankListWrapper}>
+//       <div
+//         ref={scrollRef}
+//         className={styles.RankList}
+//         onScroll={onScroll}
+//         onMouseDown={onMouseDown}
+//         onMouseUp={onMouseUp}
+//       >
+//         <RankListPage />
+//         <RankListPage />
+//         <RankListPage />
+//         <RankListPage />
+//       </div>
+//       <div className={styles.RankListDots}>
+//         {[...Array(4).keys()].map((_, index) => (
+//           <div
+//             key={index}
+//             className={`${styles.RankListDot} ${
+//               index === pageIndex ? styles.Selected : styles.UnSelected
+//             }`}
+//           ></div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
